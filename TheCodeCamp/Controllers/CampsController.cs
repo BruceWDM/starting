5780056dee095b36b791ed8abd7023d20534f9e6
BruceWDM <developer@oaksavannasoftware.com>
2020-01-09 13:11:08 -0600

    Add project files.

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+//using RouteAttribute = System.Web.Http.RouteAttribute;
+//using RoutePrefix = System.Web.Http.RoutePrefixAttribute;
+using AutoMapper;
+
+namespace TheCodeCamp.Controllers
+{
+    [RoutePrefix("api/camps")]
+    public class CampsController : ApiController
+    {
+        // GET: Camps
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper; 
+
+        public CampsController(ICampRepository repository, IMapper mapper)   
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [Route()]
+        public async Task<IHttpActionResult> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync(includeTalks);
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{moniker}", Name ="GetCamp")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                return Ok(_mapper.Map<CampModel>(result));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+                throw;
+            }
+        }
+        [Route("searchByDate/{eventDate:datetime}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> SearchByEventDate(DateTime eventDate, bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsByEventDate(eventDate, includeTalks);
+                return Ok(_mapper.Map<CampModel[]>(result));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Post(CampModel model)
+        {
+            try
+            {
+                if (await _repository.GetCampAsync(model.Moniker) != null)
+                {
+                    ModelState.AddModelError("Moniker", "Moniker in use");
+                }
+                if (ModelState.IsValid)
+                {
+                    
+                    var camp = _mapper.Map<Camp>(model);
+                    _repository.AddCamp(camp);
+                    if (await _repository.SaveChangesAsync())
+                    {
+                        var newModel = _mapper.Map<CampModel>(camp);
+                        //var location = Url.Link("GetCamp", new { moniker = newModel.Moniker });
+                        //return Created(location, newModel);
+                        return CreatedAtRoute("GetCamp", new { moniker = newModel.Moniker }, newModel);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [Route("moniker")]
+        public async Task<IHttpActionResult> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var camp = _repository.GetCampAsync(moniker);
+                if (camp == null) return NotFound();
+
+                await _mapper.Map(model, camp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(_mapper.Map<CampModel>(camp));
+                }
+                else
+                    return InternalServerError();
+
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+        [Route("moniker")]
+        public async Task<IHttpActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                if (camp == null) return NotFound();
+
+                _repository.DeleteCamp(camp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+                else
+                    return InternalServerError();
+
+
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>()
+                .ForMember(c=>c.Venue, opt => opt.MapFrom(m => m.Location.VenueName))
+                .ReverseMap();            
+            CreateMap<Talk, TalkModel>();
+            CreateMap<Speaker, SpeakerModel>();
+
+        }
+    }
+}
